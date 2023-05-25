8312651f30f5402a946fcfe00b8701cbd327d7d8
Nikolaj Sievertsen Nøring <nikosn@hotmail.dk>
2023-05-25 10:23:50 +0200

    vault

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,4 +1,30 @@
+using Catalog;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Vault vault = new();
+
+string myIssuer = vault.GetSecret("authentication", "issuer").Result;
+string mySecret = vault.GetSecret("authentication", "secret").Result;
+builder.Services
+.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = myIssuer,
+        ValidAudience = "http://localhost",
+        IssuerSigningKey =
+    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
+    };
+});
 
 // Add services to the container.
 
